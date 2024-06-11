90101f54c5e0e995b1641f6816b1b149e64ecd22
pedrobits <pedroksk2@gmail.com>
2024-06-11 11:50:15 -0300

    feat: :sparkles: add custom colors and custom hr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGreen: '#5EAC3C',
+        customGreenDark: '#4A8B30',
+        customDark: '#020617',
+        customLightBlue: '#87CEEB',
+      },
+      backgroundImage: {
+        'custom-hr': 'linear-gradient(to right, #4A8B30 18%, white 10%)',
+      },
+    },
   },
   plugins: [],
-}
+});
