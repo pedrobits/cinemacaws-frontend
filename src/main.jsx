5172a17a126bc85a5b664887a41be0a96c2d4a4f
pedrobits <pedroksk2@gmail.com>
2024-06-11 08:45:54 -0300

    feat: :tada: Iniciando projeto

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
