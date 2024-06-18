5455872b3f20ac25a7d04ea03dcad77dc233bd40
pedrobits <pedroksk2@gmail.com>
2024-06-18 14:44:07 -0300

    feat: :sparkles: change element to "comprar Ingresso"

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -3,16 +3,24 @@ const NavBar = () => {
 
 	return (
 		<>
-			<div className="flex items-center p-10 gap-10 justify-center flex-col w-screen z-50 bg-customDark shadow-2xl md:flex-row md:justify-around">
+			<div className="flex flex-col items-center justify-center p-10 gap-10 w-full z-50 bg-customDark shadow-2xl md:flex-row md:justify-around">
 				<a href="/">
 					<img className="w-56" src="/public/logo.png" alt="Logo CineMacaws" />
 				</a>
 				<div>
-					<ul className="flex items-center w-full gap-5 justify-center md:gap-10 md:justify-around">
+					<ul className="flex flex-col items-center w-full gap-5 justify-center md:gap-10 md:justify-around md:flex-row">
 						<li className={navItem}><a href="#" className="nav-link">Club Macaws</a></li>
 						<li className={navItem}><a href="#" className="nav-link">Em breve</a></li>
 						<li className={navItem}><a href="/programacao" className="nav-link">Programação</a></li>
-						<li className={navItem}><a href="#" className="nav-link">Comprar Ingresso</a></li>
+						<button
+							className="inline-flex items-center justify-center px-8 py-4 font-sans font-semibold tracking-wide text-white bg-customGreen rounded-lg h-[60px] hover:bg-customGreenDark transition-all"
+							onClick={() => {
+								window.location.href = '/comprar-ingressos';
+							}}
+						>
+							Comprar Ingressos
+						</button>
+
 					</ul>
 				</div>
 			</div>
