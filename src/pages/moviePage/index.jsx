6b8053c0378fd09af8ba9a010b5024c9ef50199e
pedrobits <pedroksk2@gmail.com>
2024-06-18 14:47:55 -0300

    feat: :sparkles: Make changes to improve responsive

--- src/pages/moviePage/index.jsx
+++ src/pages/moviePage/index.jsx
@@ -4,15 +4,19 @@ import mockDetalhesFilmes from "../../scripts/mockDetalhesFilmes.js";
 import Indicacao from "../../components/indicacaoLivre";
 import { useEffect, useState } from "react";
 import ChipGenres from "../../components/chipGenres"
+import mockSessoesPorFilme from "../../scripts/mockSessoesPorID.js"
+import Footer from "../../components/Footer/index.jsx";
 
 const MoviePage = () => {
     const { id } = useParams();
     const [filme, setFilme] = useState(null);
+    const [sessions, setSessions] = useState([]);
 
     useEffect(
         () => {
             window.scrollTo(0, 0);
             setFilme(mockDetalhesFilmes(id));
+            setSessions(mockSessoesPorFilme(id));
         },
         []
     )
@@ -32,19 +36,19 @@ const MoviePage = () => {
         <div>
             <NavBar />
             <div
-                className="relative bg-cover bg-center flex p-8 h-[40rem]"
+                className="flex flex-col h-full gap-10 relative bg-cover bg-center p-8 md:flex-row md:h-[40rem]"
                 style={{
                     backgroundImage: `url(${filme.image})`,
                 }}
             >
-                <div className="absolute inset-0 bg-black opacity-55 z-[1]"></div>
+                <div className="absolute inset-0 bg-black opacity-65 z-[1]"></div>
                 <div className="flex-shrink-0 z-20 drop-shadow-md hover:drop-shadow-xl">
                     <img src={filme.poster} alt={filme.name} className="h-auto max-w-[300px]" />
                 </div>
                 <div className="flex flex-col gap-5 ml-8 z-20">
                     <h1 className="text-4xl font-bold text-white mb-8">{filme.name}</h1>
                     <p className="text-xl text-white text-justify italic">{filme.summary}</p>
-                    <ChipGenres valor={filme.genre}/>
+                    <ChipGenres valor={filme.genre} />
                     <Indicacao
                         classification={filme.rating}
                         width="60px"
@@ -53,6 +57,7 @@ const MoviePage = () => {
                     />
                 </div>
             </div>
+            <Footer />
         </div>
     );
 };
